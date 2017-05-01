65571ca3a8f88f5691cd3738c076d5d84f44127f
Samantha Morco <samanthamorco@gmail.com>
2017-05-01 19:26:46 -0400

    arrays and hashes review

---
+++ employee_example.rb
@@ -0,0 +1,19 @@
+# arrays
+# employee1 = ['Sami', 'Morco', 1000000, true]
+# employee2 = ['Andy', 'Zhao', 100000, true]
+
+# puts employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " a year."
+# puts employee2[0] + " " + employee2[1] + " makes $" + employee2[2].to_s + " a year."
+# puts "#{employee1[0]} #{employee1[1]} makes $#{employee1[2]} a year."
+
+# hash
+employee1 = { 'last_name' => 'Morco', 'first_name' => 'Sami', 'salary' => 1000000, 'active' => true }
+# employee2 = { 'first_name' => 'Andy', 'last_name' => 'Zhao', 'salary' => 100000, 'active' => true }
+puts "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} a year."
+
+# new hash syntax
+employee3 = { :first_name => 'Andy', :last_name => 'Zhao', :salary => 100000, :active => true }
+puts "#{employee3[:first_name]} #{employee3[:last_name]} makes $#{employee3[:salary]} a year."
+
+employee4 = { first_name: 'Brian', last_name: 'Rice', salary: 1, active: true }
+puts "#{employee4[:first_name]} #{employee4[:last_name]} makes $#{employee4[:salary]} a year."
