5ddc89d112531fb3ab71054cab2fa46c26f02cf3
Samantha Morco <samanthamorco@gmail.com>
2017-05-02 19:27:33 -0400

    exercise solutions

--- manager.rb
+++ manager.rb
@@ -34,7 +34,7 @@ end
 
 class Manager < Employee
   attr_accessor :employees
-  
+
   def initialize(input_hash)
     super
     @employees = input_hash[:employees]
@@ -45,7 +45,22 @@ class Manager < Employee
     # TODO: write code to actually send out a report
     puts "Email sent!"
   end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
 end
+
+# class President < Manager
+# end
 
 employee1 = Employee.new(first_name: "Jim", last_name: "Hapley", salary: 30000, active: true)
 employee2 = Employee.new(first_name: "Pam", last_name: "Hapley", salary: 35000, active: true)
@@ -55,4 +70,9 @@ manager.print_info
 # employee.print_info
 # manager.blah
 # puts manager.full_name
-p manager.employees
+p employee1.salary
+p employee1.active
+manager.give_all_raises
+p employee1.salary
+manager.fire_all_employees
+p employee1.active
