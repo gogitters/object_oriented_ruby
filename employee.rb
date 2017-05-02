f95370cc2744b78b965d1f614b7e5343415ed6e8
Samantha Morco <samanthamorco@gmail.com>
2017-05-02 18:17:36 -0400

    add faker gem

--- employee.rb
+++ employee.rb
@@ -1,3 +1,5 @@
+require "faker"
+
 class Employee
   attr_reader :first_name, :last_name, :salary
   # attr_writer :active
@@ -10,14 +12,6 @@ class Employee
     @active = input_hash[:active] ||= false
   end
 
-  def print_info
-    puts "#{@first_name} #{@last_name} makes $#{@salary} a year."
-  end
-
-  def give_annual_raise
-    @salary = @salary * 1.05
-  end
-
   def full_name
     if @last_name[-1] == "s"
       "#{@first_name} #{@last_name}, Esquire"
@@ -26,10 +20,28 @@ class Employee
     end
   end
 
+  def print_info
+    puts "#{@first_name} #{@last_name} makes $#{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = @salary * 1.05
+  end
+
+
 end
 
 employee1 = Employee.new({active: true, last_name: "Morco", salary: 1000000})
 employee2 = Employee.new(first_name: "Andy", salary: 100000, active: true)
+
+employees = []
+100.times do
+  employees << Employee.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, salary: Faker::Number.number(5), active: true )
+end
+
+employees.each do |employee|
+  puts employee.full_name
+end
 # employee1.print_info
 # employee2.print_info
 # employee2.give_annual_raise
