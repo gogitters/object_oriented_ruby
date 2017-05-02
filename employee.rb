fdd7c02df1772a9161dcd835a7e335b5ed7b49eb
Samantha Morco <samanthamorco@gmail.com>
2017-05-01 21:29:10 -0400

    demo first two exercises

--- employee.rb
+++ employee.rb
@@ -4,10 +4,10 @@ class Employee
   attr_accessor :active
 
   def initialize(input_hash)
-    @first_name = input_hash[:first_name]
-    @last_name = input_hash[:last_name]
-    @salary = input_hash[:salary]
-    @active = input_hash[:active]
+    @first_name = input_hash[:first_name] ||= "Peter"
+    @last_name = input_hash[:last_name] ||= "Pan"
+    @salary = input_hash[:salary] ||= 1
+    @active = input_hash[:active] ||= false
   end
 
   def print_info
@@ -18,18 +18,28 @@ class Employee
     @salary = @salary * 1.05
   end
 
+  def full_name
+    if @last_name[-1] == "s"
+      "#{@first_name} #{@last_name}, Esquire"
+    else
+      "#{@first_name} #{@last_name}"
+    end
+  end
+
 end
 
-employee1 = Employee.new({active: true, last_name: "Morco", salary: 1000000, first_name: "Sami"})
-employee2 = Employee.new(first_name: "Andy", last_name: "Zhao", salary: 100000, active: true)
-employee1.print_info
-employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
-employee1.give_annual_raise
-employee1.print_info
-# p employee1
-puts employee1.first_name
-puts employee1.last_name
-employee2.active = false
-p employee2
+employee1 = Employee.new({active: true, last_name: "Morco", salary: 1000000})
+employee2 = Employee.new(first_name: "Andy", salary: 100000, active: true)
+# employee1.print_info
+# employee2.print_info
+# employee2.give_annual_raise
+# employee2.print_info
+# employee1.give_annual_raise
+# employee1.print_info
+# # p employee1
+# puts employee1.first_name
+# puts employee1.last_name
+# employee2.active = false
+# p employee2
+p employee1.full_name
+p employee2.full_name
